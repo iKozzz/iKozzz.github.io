4d280eb479682e544d8364d07f06bf40f00de534
Konstantin Alesionok <minskcitizen@gmail.com>
2014-12-17 18:24:03 +0300

    animate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $(document).ready( function() {
-    new WOW().init();
-
+    $("#hello-world ul").css('width', $(window).width());
+    $("#hello-world ul").css('height', $(window).height());
+    
+    $(window).resize(function() {
+        $("#hello-world ul").css('width', $(window).width());
+        $("#hello-world ul").css('height', $(window).height());
+    });
+    
+    $('#scene').parallax({
+          calibrateX: true,
+          calibrateY: true,
+          invertX: true,
+          invertY: true,
+          limitX: false,
+          limitY: false,
+          scalarX: 9,
+          scalarY: 7,
+          frictionX: 0.8,
+          frictionY: 0.6,
+          originX: 0.5,
+          originY: 0.5
+        }        
+    );
+    
     $('.scroll').click(function(e){
         e.preventDefault();
         var destination = $(this).attr('href');

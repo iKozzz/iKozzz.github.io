3b9750774686658742db4a7696389fb1565dab09
iKozzz <minskcitizen@gmail.com>
2015-02-10 15:56:07 +0300

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
 	"use strict";
-	
-	$('figure').hover(function () {
-		$('.eye').toggle();
-		$('figure img').toggleClass("padding");
-	});
-	
-	$('.scroll').click(function (e) {
-		e.preventDefault();
-        var destination = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(destination).offset().top}, 800);
+
+    new WOW().init();
+
+	$('figure img').hover(function () {
+		$(this).toggleClass("padding");
 	});
 });
